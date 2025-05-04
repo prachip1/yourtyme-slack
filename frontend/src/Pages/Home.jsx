64f1fb5d9b78933a5a5884e26d68b159cd7e6bf9
prachip1 <prachiscoding@gmail.com>
2025-05-04 09:39:57 +0530

    frontend changes again

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,8 +2,7 @@
 import React from 'react';
 
 function Home() {
-  const slackAuthUrl = `https://slack.com/oauth/v2/authorize?client_id=${import.meta.env.VITE_SLACK_CLIENT_ID}&scope=users:read,chat:write,commands,channels:read&redirect_uri=${encodeURIComponent('https://yourtyme-slack-backend.vercel.app/slack/oauth/callback')}`;
-
+  const slackAuthUrl = `https://slack.com/oauth/v2/authorize?client_id=${import.meta.env.VITE_SLACK_CLIENT_ID}&scope=users:read,chat:write,commands,channels:read&user_scope=users:read&redirect_uri=${encodeURIComponent('https://yourtyme-slack-backend.vercel.app/slack/oauth/callback')}`;
   return (
     <div className="home-container">
       <h1>Timezone Tool</h1>
