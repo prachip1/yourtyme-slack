ff40d6f1ca56e0f3cad3449971795c3fd6ece0eb
prachip1 <prachiscoding@gmail.com>
2025-05-09 14:51:13 +0530

    update in redirect_url

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 function Home() {
-  const slackAuthUrl = `https://slack.com/oauth/v2/authorize?client_id=${import.meta.env.VITE_SLACK_CLIENT_ID}&scope=users:read,chat:write,commands,channels:read&user_scope=users:read&redirect_uri=${encodeURIComponent('https://yourtyme-slack-backend.vercel.app/slack/oauth/callback')}`;
+  const slackAuthUrl = `https://slack.com/oauth/v2/authorize?client_id=${import.meta.env.VITE_SLACK_CLIENT_ID}&scope=users:read,chat:write,commands,channels:read&user_scope=users:read&redirect_uri=${encodeURIComponent('https://c69e-2406-b400-1a-b207-d981-d4e8-523d-bf8f.ngrok-free.app/slack/oauth/callback')}`;
   return (
     <div className="home-container">
       <h1>Timezone Tool</h1>
