bb7d144dcb8b54ef48022114630eb92b3b0a8357
prachip1 <prachiscoding@gmail.com>
2025-05-22 12:04:56 +0530

    fixing server.js

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', authRoutes);
+
+app.get('/', (req, res) => res.json({ message: 'YourTyme Backend Running' }));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
